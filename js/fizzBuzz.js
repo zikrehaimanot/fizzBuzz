851c6f664a841e2f8e323a4b5226e5abdda8c172
zikrehaimanot <zikrehaimanot@gmail.com>
2020-02-20 20:23:04 -0500

    first commit

---
+++ js/fizzBuzz.js
@@ -0,0 +1,16 @@
+for (let n = 1; n <= 100; n++) {
+  let output = "";
+  if (n % 3 == 0) output += "Fizz";
+  if (n % 5 == 0) output += "Buzz";
+  console.log(output || n);
+}
+document.getElementById("submit").onclick=alert
+
+function alert(){
+  for (let n = 1; n <= 100; n++) {
+    let output = "";
+    if (n % 3 == 0) output += "Fizz";
+    if (n % 5 == 0) output += "Buzz";
+  document.getElementsByTagName("LI").innerHTML=output
+}
+}
